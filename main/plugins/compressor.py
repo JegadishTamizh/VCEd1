cb60702d7dbec1c8824a46189d2f9c87472ffaef
JegadishTamizh <74832347+JegadishTamizh@users.noreply.github.com>
2022-04-25 11:54:08 +0530

    Update compressor.py

--- main/plugins/compressor.py
+++ main/plugins/compressor.py
@@ -93,7 +93,7 @@ async def compress(event, msg, ffmpeg_cmd=0, ps_name=None):
         cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -vf crop=1440:784:240:176 -preset slow -vcodec libx265 -crf 23 -acodec copy -c:s copy """{out}""" -y'
     elif ffmpeg_cmd == 4:
         cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -vf crop=1440:784:240:176 -preset slow -vcodec libx264 -crf 23 -acodec copy -c:s copy """{out}""" -y'
-    try:
+    #try:
         await ffmpeg_progress(cmd, name, progress, FT, edit, ps_name)
     except Exception as e:
         os.rmdir("encodemedia")
